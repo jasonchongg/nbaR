a9b010d13c08b2f9503a4d44e66a57013553904d
Jason Chong <jasonchongg@gmail.com>
2018-07-08 23:24:02 -0700

    edited player selectinput to dynamically update with season

--- app.R
+++ app.R
@@ -11,33 +11,33 @@ library(shiny)
 library(grid)
 library(jpeg)
 library(RCurl)
-library(Cairo)
-options(shiny.usecairo=T)
 source('get_data.R')
+
+# generate raster img of court background picture
 courtImg.URL <- "https://thedatagame.files.wordpress.com/2016/03/nba_court.jpg"
 court <- rasterGrob(readJPEG(getURLContent(courtImg.URL)),
                     width=unit(1,"npc"), height=unit(1,"npc"))
 
 
-# Define UI for application that draws a histogram
+# Define UI 
 ui <- fluidPage(
    
    # Application title
    titlePanel("NBA Shots Analytics"),
    
-   # Sidebar with a slider input for number of bins 
+   
+   # Sidebar with season and player inputs
    sidebarLayout(
       sidebarPanel(
-         selectInput("players",
-                     "Select Player",
-                     players$display_first_last),
          selectInput('season',
                        'Pick Season',
-                       c('2014-15', '2015-16', '2016-17'))
+                       c('2014-15', '2015-16', '2016-17')),
+         uiOutput("playerSelection")
+         
          
       ),
       
-      # Show a plot of the generated distribution
+      # Show a plot of the shots and data table
       mainPanel(
          plotOutput("shotPlot"),
          dataTableOutput('table')
@@ -49,16 +49,17 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$shotPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
+      # grab shots data
      player_id <- players$person_id[which(players$display_first_last == input$players)]
      season <- input$season
      shotsdf <- get_shots_data(player_id, season)
+     # visualize shots graph
      ggplot(shotsdf, aes(x=loc_x, y=loc_y)) +
        annotation_custom(court, -250, 250, -50, 420) +
        geom_point(aes(colour = shot_zone_basic, shape=event_type)) +
-       xlim(250, -250) +
+       xlim(250, -250) + # flip x coords for proper right/left handling
        ylim(-50, 420) +
-       coord_fixed() +
+       coord_fixed() + # fix chart to stop distortions
        ggtitle(paste('Shot Chart\n', unique(shotsdf$player_name), sep = '')) +
        theme(line = element_blank(),
              axis.title.x = element_blank(),
@@ -72,11 +73,22 @@ server <- function(input, output) {
      
    })
    
+   # render Data Set
+   
    output$table <- renderDataTable({
      player_id <- players$person_id[which(players$display_first_last == input$players)]
      season <- input$season
      shotsdf <- get_shots_data(player_id, season)
      shotsdf})
+   
+   # Make player input selection dynamic with actual players in season
+   
+   output$playerSelection <- renderUI({
+     selectInput("players", "Select Player", choices = 
+      players$display_first_last[which(players$to_year >= as.numeric(paste('20', unlist(strsplit(input$season,'-'))[2], sep = '')))],
+      selected = 'Stephen Curry')
+   })
+   
 }
 
 # Run the application 
